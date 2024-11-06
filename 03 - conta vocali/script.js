b5895bdc93abe52b12da5998fae0e7bac4005725
CarloDettori <dettoric07@gmail.com>
2024-11-06 17:59:15 +0100

    ex 3 + bonus

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -3,11 +3,43 @@
 const word = 'javascript';
 
 
+/* 
 // Dichiara la funzione qui.
-
+function vowelFilter(giveMeWord) {
+    const vowels = ["a", "e", "i", "o", "u"]
+    let vowelsNumber = 0;
+    for (let i = 0; i < giveMeWord.length; i++) {
+        for (let e = 0; e < vowels.length; e++) {
+            if (giveMeWord[i] == vowels[e]) {
+                vowelsNumber = vowelsNumber + 1;
+            }
+        }
+    }
+    return vowelsNumber
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let call = vowelFilter(word);
+console.log(call)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+*/
+
+
+// Dichiara la funzione qui.
+const vowelFilter = (giveMeWord) => {
+    const vowels = ["a", "e", "i", "o", "u"]
+    let vowelsNumber = 0;
+    for (let i = 0; i < giveMeWord.length; i++) {
+        for (let e = 0; e < vowels.length; e++) {
+            if (giveMeWord[i] == vowels[e]) {
+                vowelsNumber = vowelsNumber + 1;
+            }
+        }
+    }
+    return vowelsNumber
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(vowelFilter(word))
