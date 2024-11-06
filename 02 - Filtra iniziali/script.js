e8112b46ddba48f8ad51c7d6768d5ab43270d28d
CarloDettori <dettoric07@gmail.com>
2024-11-06 17:37:25 +0100

    ex 2 + bonus

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,50 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+let choosenLetter = "a"
+
+// Dichiara la funzione qui.
+/*
+function nameFilter(array, letter) {
+    const filteredName = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] == letter) filteredName.push(array[i]);
+    }
+    return filteredName;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+let choosenLetter = "a"
+let call = nameFilter(names, choosenLetter.toUpperCase());
+console.log(call);
+*/
+//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+
+
+
+
+
+
 
 
 // Dichiara la funzione qui.
-
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
 
-//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+// Dichiara la funzione qui.
+
+const nameFilter = (array, letter) => {
+    const filteredName = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] == letter) filteredName.push(array[i]);
+    }
+    return filteredName;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(nameFilter(names, choosenLetter.toUpperCase()));
