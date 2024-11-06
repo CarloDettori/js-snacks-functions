a3cc1241c0304ad9417bdf99609435e7a5f7154d
CarloDettori <dettoric07@gmail.com>
2024-11-06 18:10:22 +0100

    ex 4 + bonus

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
+/*
+// Dichiara la funzione qui.
+function sayHi(chooseAName) {
+    let hiTo = `Ciao ${chooseAName}!`
+    return hiTo;
+}
+// Invoca la funzione qui e stampa il risultato in console
+
+let call = sayHi(name);
+console.log(call);
+*/
 
 
 // Dichiara la funzione qui.
-
+const sayHi = (chooseAName) => {
+    let hiTo = `Ciao ${chooseAName}!`
+    return hiTo;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(sayHi(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
